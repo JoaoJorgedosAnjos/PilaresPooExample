47debcf586bb2fd1578126141e537ad874dcc150
joaojorge <joaojorgedosanjos@gmail.com>
2023-06-25 08:00:56 -0300

    Primeiro commmit

---
+++ src/Autodromo.java
@@ -0,0 +1,5 @@
+public class Autodromo {
+    public static void main(String[] args) {
+        System.out.println("Opa");
+    }
+}
---
+++ src/Carro.java
@@ -0,0 +1,18 @@
+public class Carro {
+
+    private String nome;
+    private String marca;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public Carro(String nome, String marca) {
+        this.nome = nome;
+        this.marca = marca;
+    }
+}
